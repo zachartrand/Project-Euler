98eff4cd5f06d743cb1395bfc74a46417dfa0d3e
Zachary Chartrand <zachartrand999@gmail.com>
2021-09-29 15:49:11 -0400

    Initial commit of problems 18, 19, 27, and 67.

---
+++ Solutions/problem_19_counting_sundays.py
@@ -0,0 +1,37 @@
+"""
+@author: Zach Chartrand <zachartrand999@gmail.com>
+
+Project Euler Problem 19: https://projecteuler.net/problem=19
+
+Counting Sundays
+    You are given the following information, but you may prefer to do some research for yourself.
+
+        * 1 Jan 1900 was a Monday.
+        * Thirty days has September,
+          April, June and November.
+          All the rest have thirty-one,
+          Saving February alone,
+          Which has twenty-eight, rain or shine.
+          And on leap years, twenty-nine.
+        * A leap year occurs on any year evenly divisible by 4, but not on a
+          century unless it is divisible by 400.
+
+    How many Sundays fell on the first of the month during the twentieth
+    century (1 Jan 1901 to 31 Dec 2000)?
+"""
+
+from datetime import date
+
+
+def main():
+    count = 0
+    for year in range(1901, 2001):
+        for month in range(1, 13):
+            if date(year, month, 1).weekday() == 6:
+                count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    print(main())  # 171
